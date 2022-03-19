091339b44f4095f387b14ca3a83a76a09dca38c8
Anton Pushkarev <gridmaniac@gmail.com>
2022-03-20 02:18:39 +0900

    change default port

--- modules/server.js
+++ modules/server.js
@@ -4,6 +4,6 @@ const app = new express();
 const router = require("../routers/v1");
 
 app.use("/api/v1/", router);
-app.listen(process.env.PORT || 3000, () => {
+app.listen(process.env.PORT || 8080, () => {
   console.log("Server has started.");
 });
