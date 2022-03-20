1fb8237dee931023ec4e114fa6ac7370ae0c6200
Anton Pushkarev <gridmaniac@gmail.com>
2022-03-20 17:06:59 +0900

    add cors for dev env

--- modules/server.js
+++ modules/server.js
@@ -3,6 +3,9 @@ const express = require("express");
 const app = new express();
 const router = require("../routers/v1");
 
+const cors = require("cors");
+if (process.env.DEVELOPMENT) app.use(cors());
+
 app.use("/api/v1/", router);
 app.listen(process.env.PORT || 8080, () => {
   console.log("Server has started.");
