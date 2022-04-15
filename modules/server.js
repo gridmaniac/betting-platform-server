1508df4c4c4d3130f080cf76c7c71faa87fc386b
Anton Pushkarev <gridmaniac@gmail.com>
2022-04-15 22:10:01 +0900

    add express form-data handler

--- modules/server.js
+++ modules/server.js
@@ -5,7 +5,9 @@ const router = require("../routers/v1");
 
 const cors = require("cors");
 if (process.env.DEVELOPMENT) app.use(cors());
+
 app.use(express.urlencoded());
+app.use(express.json());
 
 app.use("/api/v1/", router);
 app.listen(process.env.PORT || 8080, () => {
