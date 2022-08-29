67745b6019bcfedd5a5e359eab75d85fedf469fc
Anton Pushkarev <gridmaniac@gmail.com>
2022-08-29 22:24:58 +0900

    fix jobs

--- index.js
+++ index.js
@@ -3,11 +3,12 @@ else require("./modules/db");
 
 require("./modules/auth");
 require("./modules/server");
-require("./modules/jobs");
 
 process.on("uncaughtException", function (err) {
   console.error(err);
 });
+
+if (!process.env.DISABLE_JOBS) require("./modules/jobs").runJobs();
 
 if (!process.env.DISABLE_DEPOSITS_MODULE)
   require("./modules/deposits").runDeposits();
